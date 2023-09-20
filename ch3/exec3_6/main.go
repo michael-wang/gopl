6b7e2e701a07c8fc0205bcf527386453329728cc
Michael Wang <michael.wang@hey.com>
2023-09-20 16:29:32 +0800

    Exercise 3.6

--- ch3/exec3_6/main.go
+++ ch3/exec3_6/main.go
@@ -22,17 +22,59 @@ func main() {
 	)
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	for py := 0; py < height; py++ {
-		y := float64(py)/height*(ymax-ymin) + ymin
+		// y := float64(py)/height*(ymax-ymin) + ymin
+		y0 := (float64(py)-0.5)/height*(ymax-ymin) + ymin
+		y1 := (float64(py)+0.5)/height*(ymax-ymin) + ymin
+		y2 := (float64(py)-0.5)/height*(ymax-ymin) + ymin
+		y3 := (float64(py)+0.5)/height*(ymax-ymin) + ymin
 		for px := 0; px < width; px++ {
-			x := float64(px)/width*(xmax-xmin) + xmin
-			z := complex(x, y)
-			img.Set(px, py, mandelbrot(z))
+			// x := float64(px)/width*(xmax-xmin) + xmin
+			x1 := (float64(px)-0.5)/width*(xmax-xmin) + xmin
+			x0 := (float64(px)+0.5)/width*(xmax-xmin) + xmin
+			x2 := (float64(px)-0.5)/width*(xmax-xmin) + xmin
+			x3 := (float64(px)+0.5)/width*(xmax-xmin) + xmin
+
+			// z := complex(x, y)
+			z0 := complex(x0, y0)
+			z1 := complex(x1, y1)
+			z2 := complex(x2, y2)
+			z3 := complex(x3, y3)
+
+			c0 := mandelbrot(z0)
+			c1 := mandelbrot(z1)
+			c2 := mandelbrot(z2)
+			c3 := mandelbrot(z3)
+
+			img.Set(px, py, avgColor([]color.Color{c0, c1, c2, c3}))
 		}
 	}
 	png.Encode(os.Stdout, img)
 
 	fmt.Fprintf(os.Stderr, "rBound: %v\n", rBound)
 	fmt.Fprintf(os.Stderr, "iBound: %v\n", iBound)
+}
+
+func avgColor(colors []color.Color) color.Color {
+	var avgR, avgG, avgB, avgA uint32
+	for _, c := range colors {
+		r, g, b, a := c.RGBA()
+		avgR += r
+		avgG += g
+		avgB += b
+		avgA += a
+	}
+	n := uint32(len(colors))
+	avgR /= n
+	avgG /= n
+	avgB /= n
+	avgA /= n
+
+	return color.RGBA{
+		R: uint8(avgR >> 8),
+		G: uint8(avgG >> 8),
+		B: uint8(avgB >> 8),
+		A: uint8(avgA >> 8),
+	}
 }
 
 func mandelbrot(z complex128) color.Color {
